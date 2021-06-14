be221de49673002813818d153a55f5ffeaf81444
Aseel-Z <aseelalzweri@gmail.com>
2021-06-14 08:52:46 +0300

    --2--

--- insertion_sort.py
+++ insertion_sort.py
@@ -3,8 +3,8 @@ def InsertionSort(arr):
         j= i-1
         temp = arr[i]
 
-    while(j >= 0 and temp < arr[j]):
-        arr[j + 1] = arr[j]
-        j = j - 1
-        
-      arr[j + 1] = temp
+        while(j >= 0 and temp < arr[j]):
+                arr[j + 1] = arr[j]
+                j = j - 1
+                
+        arr[j + 1] = temp
